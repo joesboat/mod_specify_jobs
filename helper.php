9ffb92c2d8d03fb9950e60d416ecc863fa4ceecd
JOSEPH P GIBSON <joe@joesboat.org>
2020-06-06 12:19:05 -0400

    Fix issues found by Penn

--- helper.php
+++ helper.php
@@ -16,36 +16,31 @@ require_once(JPATH_LIBRARIES."/usps/tableD5VHQAB.php");
 require_once(JPATH_LIBRARIES."/usps/dbUSPSd5WebSites.php");
 class modspecify_jobsHelper
 {
-//*************************************************************
-static function check_permissions($setup, $mode, $user_id){
+static function check_permissions($setup, $mode){
 require(JPATH_LIBRARIES."/USPSaccess/dbUSPS.php");
-	$ok = false;
-	$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
-	$jobs = $vhqab->getJobsObject();
-	$exc = $vhqab->getExcomObject();
-	$blob = JoeFactory::getTable("tableD5Blobs",$db_d5);
+$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+$jobs = $vhqab->getJobsObject();
+$exc = $vhqab->getExcomObject();
+$blob = JoeFactory::getTable("tableD5Blobs",$db_d5);
 	$year = $blob->get_manage_year();	
-//	if (isset($_GET['mode'])){
-//		$_SESSION['mode'] = strtolower($_GET['mode']);
-//		if ($_SESSION['mode'] == 'squad')
-//			$_SESSION['squad_no'] = $vhqab->getSquadNumber($username);
-//		else
-//			$_SESSION['squad_no'] = '6243';
-//	}
+	$user = JFactory::getUser();
+	$user_id = $user->username;
 	$setup['user_id'] = $user_id;
+	$setup['mode'] = $mode;
 	switch($mode){
 		case 'd5':
 			$setup['dept_code']="";
 			$setup['excom'] = $exc->get_highest_permission($setup['user_id'], $year) ;
-			$setup['squad'] = '6243' ;
+			$setup['excom']['member_name'] = $vhqab->getMemberNameAndRank($setup['excom']['certificate']);
+			$setup['squad_no'] = '' ;
 			$setup['dept_code'] = $exc->get_permission($setup['excom']);
 			$setup['header'] = "D5 Job Assignments for $year. - ".$setup['excom']['excom_position']." Permissions";
 			$setup['unit_name'] = 'D5';
+			$setup['order'] = "jobcode" ;
+			$setup['cow'] = $vhqab->getSquadronCOW('6243');
 			break; 
 		default:
-			//if ($loging) log_it("site_maint is  $site_maint");  
-			$setup['squad_no'] = $vhqab->getSquadNumber($user_id);
-			if (! $exc->excom_member($user_id,"webmaster",$year)){
+			if (! $exc->excom_member($user_id,"site_maint",$year)){
 				//if (! $exc->excom_member($user_id,"site_maint",$year)) 
 				if (!($jobs->check_squadron_officer($_SESSION['user_id'],sprintf("%04d",$setup['squad_no']),$year)))
 				{
@@ -54,50 +49,83 @@ require(JPATH_LIBRARIES."/USPSaccess/dbUSPS.php");
 				log_it("Member ".$setup['user_id']." was not allowed to use this tool") ;
 				exit(0);
 				}
-			
 			}
-			$setup['squad'] = $mode;
+			if (isset($_POST['new_squad'])){
+				// only possible if this is the 2nd loop through check permissions
+				$setup['squad_no'] = $_POST['new_squad'];
+				$setup['next'] = 'jobs';
+			} else 
+				$setup['squad_no'] = $vhqab->getSquadNumber($user_id);
 			$setup['dept_code'] = '31000'; 
 			$setup['unit_name'] = $vhqab->getSquadronName(sprintf("%04d",$setup['squad_no']));
+			$setup['cow'] = $vhqab->getSquadronCOW($setup['squad_no']);
 			$setup['header'] = "Squadron Job Assignments for $year. - ".$setup['unit_name'];
 			$setup['order'] = "update_order" ;
 			break;
 	}
 	$_SESSION['when_done'] = $_SERVER['PHP_SELF'];
 	$setup['site_maint'] = $site_maint = $exc->excom_member($user_id,"site_maint",$year);
-	$setup['site_url'] = "http://".$_SERVER['SERVER_NAME'].$_SERVER['CONTEXT_PREFIX'];
+	$setup['site_url'] = getSiteUrl();
 	$setup['year'] = $year;
+	$dt = date("Y-m-d");
 	return $setup;
 }
 //*************************************************************
-static function display_introduction($setup){
-	$str = "<table class='table table-bordered'>";		// Main display table 
-	$str .= "<colgroup><col id='xc1'><col id='xc2'></colgroup>";
-	$str .=  "<tr><td colspan='2'>";
-	$str .=  "<p>";
-	if ($setup['squad'] == '6243'){
-		$str .= xshow_d5_introduction($setup['excom'],$setup);
-	} else {
-		$str .= xshow_squad_introduction($setup);
+static function get_d5_jobs_list($dept,$order="jdesc"){
+$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+$codes = $vhqab->getJobcodesObject();
+	// join jobdesc, jobs, members
+	// omit jobdesc ending in 2 (committee members)
+	// 
+	$query = '';
+	$rows=array();
+	if ($dept != ""){
+		$query .= "(";
+		$r = substr($dept,4,1);	
+		if ($r == '0'){
+			$query .= "jobcodes.department = '";
+			$query .= substr($dept,0,4)."1'";
+			$query .= " or ";	
+		}
+		$query .= "jobcodes.department = '$dept'";
+		$query .= ") and ";
 	}
-	$str .=  "<br>Press <input type='submit' name='command' value='Continue' /> ";
-	$str .=  " to display the Jobs List.";
-	$str .=  "</p>";
-	$str .=  "</td></tr>";
-	$str .=  "</table>";
-	return $str;
+	$query .= "("; 
+	$query .= "jobcode LIKE '2____' ";
+	$query .= ")";
+	$query .= "and d5_job = 1";
+	$jcs = $codes->search_records_in_order($query,$order);
+	return $jcs; 
+}
+//*********************************************************
+static function get_jobcode_record($code){
+	$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+	$codes = $vhqab->getJobcodesObject();
+	return $codes->get_record("jobcode",$code);
+}
+//*********************************************************
+static function get_member_list($unit){
+	$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+	$mbr = $vhqab->getD5MembersObject();
+	return	$mbr->get_d5_or_squad_member_list($unit);
 }
 //*************************************************************
-static function handle_command(){
+static function get_squadron_jobs_list($order){
+$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+$codes = $vhqab->getJobcodesObject();
+	return $codes->get_squad_assignments($order);	
+}
+//*************************************************************
+static function get_squadron_list(){
+	$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+	return $vhqab->getD5SquadronList();
+}
+//*************************************************************
+static function handle_command($setup){
 	$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
 	$codes = $vhqab->getJobcodesObject();
 	$jobs = $vhqab->getJobsObject();
 	$cmd = strtolower($_POST['command']);
-	if (isset($_SESSION['setup'])){
-		$setup = json_decode($_SESSION['setup'],TRUE);
-	} else {
-		$setup = array();
-	}
 	switch($cmd){
 		case 'cancel':
 		case 'finished':
@@ -115,7 +143,10 @@ static function handle_command(){
 				case 'committee':
 				 	$jobs->update_committee($_POST, $setup['year']);
 					break;
-				case 'jobs': 
+				case 'jobs':
+					if ($setup['cow'] != $_POST['cow_date']){
+						$setup['cow'] = modspecify_jobsHelper::update_cow_date($_POST);
+					}
 					$jobs->update_jobs($_POST, $setup['year']);
 					break;
 				case 'jobcode':
@@ -129,13 +160,6 @@ static function handle_command(){
 			}
 			break;
 		case 'continue':
-			// = $_POST['order'];
-			if (isset($_POST['squad']))
-				if ($_POST['squad'] != $setup['squad']){
-					$setup['squad_no'] = $setup['squad'] = $_POST['squad'];
-					$setup['unit_name'] = $vhqab->getSquadronShortName(sprintf("%04d",$_POST['squad']));
-					$setup['header'] = "Squadron Job Assignments for ".$setup['year']." - ".$vhqab->getSquadronName($setup['squad']);
-				}
 			break;
 		case 'create':			// Only valid for Commander and Dept. Heads
 			$new_committee = $codes->get_new_code();
@@ -149,12 +173,99 @@ static function handle_command(){
 		$updating=$_POST['updating'];
 		$setup['next'] = $_POST['next'];
 		if (isset($_POST['committee_code']))
-			$committee_code = $_POST['committee_code'];
+			$setup['committee_code'] = $_POST['committee_code'];
 		if (isset($_POST['jobcode']))
-			$jobcode = $_POST['jobcode'];
+			$setup['jobcode'] = $_POST['jobcode'];
 	}
 	$vhqab->close();
 	return $setup;
 }
+//*********************************************************
+static function populate_committee_members($jobcode,$squad_no,$yr){
+$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+$codes = $vhqab->getJobcodesObject();
+$mbr = $vhqab->getD5MembersObject();
 
+return $vhqab->populateGroupOrCommittee($jobcode,$yr,$squad_no,0);
+
+	// Create an array containing members of this committee
+	$committee=$codes->get_record("jobcode",$code);
+	if ($committee["committee"] == "1"){
+		$emer_code = $code + 9;
+		$asst_code = $code + 1;
+		$mem_code = $code + 2;
+		$emeritus = $vhqab->getJobAssignments($emer_code,$setup['year'],$squad_no);
+		$chairs = $vhqab->getJobAssignments($code,$setup['year'],$squad_no);
+		$asst_chairs = $vhqab->getJobAssignments($code+1,$setup['year'],$squad_no);
+	} else 
+		$mem_code = $code;
+	$members=$vhqab->getJobAssignments($mem_code,$setup['year'],$squad_no);
+}
+//*********************************************************
+static function populate_jobs_list($job_list, $setup){
+$list = array();
+$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+	foreach($job_list as $jc){
+		$jdesc = $jc['jdesc'];
+		if ($jc['jobcode'] == 28071){
+			$xyz = $jc['jobcode'];
+		}
+		switch($jc['committee']){
+			case 0:			// Named Jobs
+			case 1:			// Traditional Committee
+				$rows = $vhqab->getJobAssignments($jc['jobcode'],$setup['year'],$setup['squad_no'],$jc['committee']==1);
+				if (count($rows) == 0){
+					$new = $jc;
+					$new['name'] = $name = ''; 
+					$new['cert'] = '';
+					$list[] = $new;
+					break;					
+				} else				
+				foreach($rows as $row){
+					$cert = $row['certificate'];
+					$new = $jc;
+					$new['name'] = $name = $vhqab->getMemberNameAndRank($row['certificate']); 
+					$new['cert'] = $row['certificate'];	
+					$list[] = $new;
+				} 
+				
+				break;
+			case 2:			// Group 
+				$new = $jc;
+				$new['name'] = $name = ''; 
+				$new['cert'] = '';				
+				$list[] = $new;
+				break;
+			case 4:			// Officer 
+				// it's a bridge level job.
+				$row = $vhqab->getExcomMember($jc['jobcode'],$setup['year']);
+				if ( ! $row ){
+					$new = $jc;
+					$new['name'] = $name = ''; 
+					$new['cert'] = '';
+					$list[] = $new;
+					break;					
+				}
+				$new = $jc;
+				$new['name'] = $name = $vhqab->getMemberNameAndRank($row['certificate']); 
+				$new['cert'] = $row['certificate'];
+				$list[] = $new;
+				break;
+			default:
+				continue 2;
+		}
+		//$list[] = $new;
+
+	}
+	return $list;
+}
+//*********************************************************
+static function update_cow_date($pst){
+	$vhqab = JoeFactory::getLibrary("USPSd5tableVHQAB");
+	$sqds = $vhqab->getSquadronObject();
+	$row=array('cow_date'=>$pst['cow_date'],'squad_no'=>$pst['squad_no']);
+	$sqds->update_record_changes('squad_no',$row);
+	return $pst['cow_date'];
+}
 } // End of Class modspecify_jobsHelper
+
--- tmpl/main.php
+++ tmpl/main.php
@@ -10,27 +10,61 @@
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-$doc = JFactory::getDocument();
-$doc->addScript("scripts/JoesSlideShow.js");
+$document = JFactory::getDocument();
+$document->addStyleSheet(getSiteUrl()."/plugins/system/t3/base/bootstrap/css/bootstrap-responsive.css");
+$document->addStyleSheet(getSiteUrl()."/templates/usps-site/css/bootstrap-datepicker3.css");
+$document->addStyleSheet(getSiteUrl()."/templates/usps-site/css/bootstrap.css");
+$document->addScript(getSiteUrl()."/plugins/system/t3/base/js/jquery-1.11.2.js");
+$document->addScript(getSiteUrl()."/plugins/system/t3/base-bs3/bootstrap/js/bootstrap.js");
+$document->addScript(getSiteUrl()."/templates/usps-site/js/bootstrap-datepicker.js");
+$document->addScript("scripts/JoesSlideShow.js");
+?>
+<style type="text/css">
+.datepicker {
+	background-color: #fff ;
+	color: #333 ;
+}
+</style>
+		<script>
+			$(function(){
+		   $(".datepicker").datepicker({
+			format: "yyyy-mm-dd",
+			orientation: "bottom auto",
+			autoclose:"TRUE",
+			todayHighlight: true
+    			});
+			});
+function on_file(){
+var form = (document.getElementById("fh1"));
+var file = document.getElementById('in_file');
+var gross_name = file.value;
+var a_gross = gross_name.split("\\");
+var file_name = a_gross[a_gross.length-1];
+var subTotalField = document.getElementById("subTotalField1");
+
+}
+		</script>
+<?php
 	showHeader($setup['header'],$_SERVER['REQUEST_URI']);
 $site_url = $setup['site_url'];
 
 //*************************************************************
 // display the header row 
 $name = $setup['unit_name'];
-$_SESSION['setup'] = json_encode($setup);
-$order = array(	'jdesc'=>'Job Description',
+// $_SESSION['setup'] = json_encode($setup);
+$order = array	(
+				'jdesc'=>'Job Description',
 				'jobcode'=>"Job Code",
 				'display_order'=>'Display Order',
-				'update_order'=>"Update Order");
-	$dept = $setup['dept_code'];
-	if ($dept == 21000 || $dept == 25710 || $dept == 25000 || $dept == 25001) 
-		$dept = "";
+				'update_order'=>"Update Order"
+				);
+
 ?>
 <section style="display: table;">
   <header style="display: table-row;">
 	<div style="display:table-cell;" class="center"><?php echo $name ?>
-		Jobs - District Year: <?php echo $setup['year']; ?>
+		Jobs - District Year: <?php echo $setup['year']; ?>  
+		Change of Watch on <input type='text' name='cow_date' class="datepicker" size='10' value='<?php echo $setup['cow'];?>' >
 	</div>
 	<div style="display:table-cell;" class="center">
 		<?php echo $name ?> Members
@@ -63,51 +97,46 @@ $order = array(	'jdesc'=>'Job Description',
 -->  
   <div style="display: table-row;">
 	<div style="display: table-cell; vertical-align: top;">
+				<p>Reorder to: 
+<?php		
+					foreach($order as $col=>$nm){
+						if ($setup['squad_no'] == '6243'){
+							if ($col == 'update_order') continue;
+							if ($col == 'display_order') continue;
+						}	
+?>
+						<input type="radio" name='new_order' 
+							value='<?php echo $col;?>' 
+							<?php if ($setup['order'] == $col) echo 'checked'; ?>
+							onclick='submit_all(this)' />
+						<?php echo $nm;?>
+<?php		
+					}
+?>	
+				</p>
 		<div  style = "height: 400px; overflow: auto;">
 			<input type='hidden' name='updating' value='jobs' >  
 			<!--Always plan to return here after update--> 
 			<input type='hidden' name='next' id='next' value='jobs' >
-			<input type='hidden' name='squad_no' id='squad_no' value='<?php echo $setup['squad']; ?>' >
+			<input type='hidden' name='squad_no' id='squad_no' value='<?php echo $setup['squad_no']; ?>' >
 			<!--Obtain data to display job assignments -->
 
 			<!--Begin display-->
 			<table id='tbl2'>	
-<?Php
-	if ($setup['squad']=='6243'){
-		$list = $codes->get_active_d5_committes($dept);
-	}else{
-?>
-		<p>Reorder to: 
-<?php		
-		foreach($order as $col=>$nm){
-?>
-		<input type="radio" name='new_order' value='<?php echo $col;?>' 
-			<?php if ($setup['order'] == $col) echo 'checked'; ?>
-			onclick='submit_all(this)' />
-		<?php echo $nm;?>
-<?php		
-		}
-?>	
-		</p>
 <?php
-		$list = $codes->get_squad_assignments($setup['order']);
-	}			
-	show_jobs_list($list,$setup['squad'],$setup['year'], $setup, $vhqab, $exc);
-?>
-			</table>
+$id = 0;
+	foreach($JobList as $jobrow){
+		$id++;
+		$cert = $jobrow["cert"];
+		$name = $jobrow['name'];
+		show_job_row($id,$jobrow['committee'],$jobrow['jobcode'],$jobrow['jdesc'],$name,$setup['site_url'],$setup,$cert);}
+	?></table>
 		</div>
 	</div>
     <div style="display: table-cell; vertical-align: top;">
 		<div>   
-<?php
-			if ($setup['squad']=='6243')
-				{$unit = '';}
-			else
-				{$unit = $setup['squad'];}
-			$list = $mbr->get_d5_or_squad_member_list($unit);
-?>
 				<select name='member_cert' size='23' id='member_cert' >" ;
-					<?php echo get_option_list($list,"");?>
+					<?php show_option_list($mbr_list,"");?>
 				</select>
 		</div> 
 	</div>
@@ -122,136 +151,54 @@ $order = array(	'jdesc'=>'Job Description',
 echo "<p>Otherwise, use menu's to select a different page. </p>";
 showTrailer();
 //*************************************************************
-function show_jobs_list($list,$squad,$year,$setup, $vhqab, $exc){
-$id = 0;
-	foreach($list as $jc){
-		if ($jc['jobcode'] == 22600){
-			$jjj = $jc;
-		}
-		$jdesc = $jc['jdesc'];
-		$jobcode = $jc['jobcode'];
-		if ($jc['committee']==3)		// Not used
-			continue;
-		if ($jc['committee']==4){
-			// it's a bridge level job.
-			$row = $vhqab->getExcomMember($jc['jobcode'],$year);
-			if ( ! $row ) continue;
-			
-			$name = $vhqab->getMemberNameAndRank($row['certificate']); 
-			show_job_row($id+=1,$jc['committee'],$jobcode,$jdesc,$row['certificate'],$name,$setup['site_url'],$setup);
-			continue ;
-		}
-		if ($jc['committee']==1){
-			$rows = $vhqab->getJobAssignments($jc['jobcode'],$year,$squad,TRUE);
-		} else {
-			$rows = $vhqab->getJobAssignments($jc['jobcode'],$year,$squad);
-		}
-		if (count($rows)>0 and $jc['committee'] < 2){
-			// For Named Jobs or Traditional Committees	
-			foreach($rows as $row){
-				$cert = $row['certificate'];
-				switch ($jc['committee']){
-					case 0:
-					case 1:
-						$name = $vhqab->getMemberNameAndRank($row['certificate']); 
-						break; 
-					default:
-						$column_data[2] = "Select 'Show' to list." ; 	
-				}
-				show_job_row($id+=1,$jc['committee'],$jobcode,$jdesc,$cert,$name,$setup['site_url'],$setup);
-			}
-		}else
-			show_job_row($id+=1,$jc['committee'],$jobcode,$jdesc,'','',$setup['site_url'],$setup);
-	}
-
-}
 //*************************************************************
-function show_job_row($id,$committee,$jobcode,$jdesc,$cert,$name,$site_url,$setup){
+function show_job_row($id,$committee,$jobcode,$jdesc,$name,$site_url,$setup,$cert){
 	if ($committee==3) 
 		$style = " style='color:grey;' ";
 	elseif (substr($jobcode,2,3) == '000')
 		$style = " class='lead' ";
 	else	
 		$style = "";
-	$col3 = $col4 = '';
-	$col2 = "<input type='hidden' id='cert_$id' value='$cert' /> . $name" ;
-	switch ($committee){
-		case 0:
-			$col3 =	"<button type='button' id=del_$id onclick='btn_del_job_assignment($id);' ".
-					"title='Delete the member shown from this position' >".
-					"<img src='$site_url/images/close.gif'>".
-					"</button>";
-			$col4 = "<button type='button' id=add_$id ".
-					"onclick='btn_add_job_assignment($id);' ".
-					"title='Assign this job to a D5 Member. First select the member from the D5 Roster.  ".
-					"A new job entry will be created if a member is already assigned to the job.'>".
-					"<img src='$site_url/images/left.gif' width='30'>".
-					"</button>";
-			break;
-		default:
-			$col3 =	"<button type='button' id='cmte_$id' onclick='btn_show_committee($id);' ".
-					"title='Switches focus to the Specify Committee Assignments Page.'>".
-					"Show".
-					"</button>";
-	}
-	$col5 = "<button type='button' id='define_$id' ".
-			"onclick='btn_define_jobcode($id);' ".
-			"title='Switches focus to the Setup Jobcode Description Page ".
-			"where you may modify the name and parameters of this jobcode.' >".
-			"<img src='$site_url/images/down.gif' width='30' ".
-			"</button>";
-
-	if ($committee == 4) 
-		$col3 = $col4 = $col5 = '';
-?>
-	<tr id='tr_<?php echo $id;?>'>
+?><tr id='tr_<?php echo $id;?>'>
 	<input type='hidden' id='jobcode_<?php echo $id;?>' value='<?php echo $jobcode;?>' />
-	<input type='hidden' id='cert_<?php echo $id;?>' value='<?php echo $cert;?>' />
 	<!--Display column 1  (Job or Committee Name)-->
-	
 	<td id='c1_<?php echo $id;?>' <?php echo $style;?> >
 	<?php echo $jdesc;?>
 	</td>
-	
+	<input type='hidden' id='cert_<?php echo $id;?>' value='<?php echo $cert;?>' />
 	<!--Display column 2-->  
 	<td id='c2_<?php echo $id;?>'><?php echo $name;?></td>
-
-	<!--Display column 3-->  
+	<!--Display column 3-->
 	<td id='c3_<?php echo $id;?>'>
 <?php 
 		if ($committee != 4)
-		if ($committee == 0){
-?>			
+		if ($committee == 0)
+		{
+?>
 			<button type='button' id='del_<?php echo $id;?>' onclick='btn_del_job_assignment(<?php echo $id;?>)' title='Delete the member shown from this position' >
 				<img src='<?php echo $site_url;?>/images/close.gif'>
 			</button>
 <?php
 		} else {
 ?>
-			<button type='button' id='cmte_<?php echo $id;?>' onclick='btn_show_committee(<?php echo $id;?>);' title='Switches focus to the Specify Committee Assignments Page.'>
-				Show
-			</button>
-
+			<button type='button' id='cmte_<?php echo $id;?>' onclick='btn_show_committee(<?php echo $id;?>);' title='Switches focus to the Specify Committee Assignments Page.'>Show</button>
 <?php			
 		}
 ?>
 	</td>
-
 	<!--Display column 4-->  
 	<td id='c4_<?php echo $id;?>'>
 <?php 		
 		if ($committee != 4)
 		if ($committee == 0){
-?>			
-			<button type='button' id='add_<?php echo $id;?>' onclick='btn_add_job_assignment(<?php echo $id;?>);' title='Assign this job to a D5 Member. First select the member from the D5 Roster. A new job entry will be created if a member is already assigned to the job.'>
-				<img src='<?php echo $site_url;?>/images/left.gif' width='30'>
-			</button>
+?>
+			<button type='button' id='add_<?php echo $id;?>' onclick='btn_add_job_assignment(<?php echo $id;?>);' title='Assign this job to a D5 Member. First select the member from the D5 Roster. A new job entry will be created if a member is already assigned to the job.'><img src='<?php echo $site_url;?>/images/left.gif' width='30'></button>
 <?php
 		}
 ?>		
-	</td>		
+	</td>
 	<!--Display column 5-->
-<?php 
+<?php
 	if ($setup['site_maint'] and $committee != 4){
 ?>
 		<td id='c5_<?php echo $id;?>'>
@@ -261,7 +208,5 @@ function show_job_row($id,$committee,$jobcode,$jdesc,$cert,$name,$site_url,$setu
 		</td>
 <?php	
 	}
-?>
-	</tr>
-<?php	
+?></tr><?php
 }
